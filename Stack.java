f04289c224e446a4c02c880bf8a16b9bfcaedd95
Darren Barnett <db486@sussex.ac.uk>
2025-09-05 12:16:41 +0100

    Added comments

--- Stack.java
+++ Stack.java
@@ -1,7 +1,24 @@
 public interface Stack<T> {
+    /**
+     * Pushes an item onto the stack.
+     */
     void push(T item);
+    /**
+     * Pops an item off the stack.
+     */
     T pop();
+    /**
+     * Peeks at the top item of the stack without removing it.
+     */
     T peek();
+    /**
+     * Checks if the stack is empty.
+     * @return true if the stack is empty, false otherwise
+     */
     boolean isEmpty();
+    /**
+     * Returns the number of items in the stack.
+     * @return the number of items in the stack
+     */
     int size();
 }
